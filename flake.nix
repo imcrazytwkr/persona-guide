a0247838f282300de9e11fc6962f246a8560854d
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-08-30 20:07:19 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Persona guide development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt);
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.just
+            pkgs.nodejs_24
+          ];
+          shellHook = ''
+            PS1='\u@persona-guide-dev:\w/ > '
+            export PS1
+          '';
+        };
+      });
+    };
+}
