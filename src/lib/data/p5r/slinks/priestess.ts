51e617ddc64bd2c91a60bb92c3da278bfd7d3118
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-09-18 17:23:07 +0300

    Fixed Priestess slink (P5R)

--- src/lib/data/p5r/slinks/priestess.ts
+++ src/lib/data/p5r/slinks/priestess.ts
@@ -7,17 +7,17 @@ export const priestess: SocialLink = {
 		value: 'priestess'
 	},
 	name: 'Makoto Niijima',
-	location: 'Student Council Office or school gates',
+	location: 'Shujin Academy 3F in front of the student council room; school gates on days off',
 	unlock:
-		'- After clearing the Bank Palace, find Makoto in front of the Student Council Office at school.\n- Knowledge 3 (Scholarly)',
-	availability: 'Daytime on Tuesday, Thursday, Saturday and Sunday; day and night on rainy days.',
+		'- After 6/24, talk to Makoto in front of the student council room.\n- Knowledge 3 (Scholarly)',
+	availability: 'Daytime on Tuesday, Thursday, Saturday, and Sunday; day and night on rainy days.',
 	ranks: [
 		{
 			rank: 1,
 			requirements: '',
 			choices: [
 				{
-					prompt: "I've had all kinds of new experiences today… and it's even been a little fun.",
+					prompt: "I've had all kinds of new experiences today... and it's even been a little fun.",
 					options: [
 						{ text: "Let's go again sometime.", points: 3 },
 						{ text: "It's a new you.", points: 3 },
@@ -29,7 +29,7 @@ export const priestess: SocialLink = {
 		},
 		{
 			rank: 2,
-			requirements: '',
+			requirements: 'Unavailable when raining.',
 			choices: [
 				{
 					prompt: 'I heard there were quite a few brothels as well.',
@@ -38,16 +38,17 @@ export const priestess: SocialLink = {
 				{
 					prompt: "If you hadn't been there to rescue me, I...",
 					options: [
-						{ text: 'You should have known better.', points: 2 },
-						{ text: 'That was dangerous.', points: 3 }
+						{ text: 'That was dangerous.', points: 3 },
+						{ text: 'You should have known better.', points: 2 }
 					]
 				},
 				{
-					prompt: 'Phone (P5R): I guess nothing beats practical experience.',
-					options: [{ text: "Couldn't agree more.", points: 3 }]
+					prompt:
+						'Today was exhausting, but I also learned a lot. I guess nothing beats practical experience.',
+					options: [{ text: 'Couldn’t agree more.', points: 2 }]
 				}
 			],
-			unlocks: ['Baton Pass']
+			unlocks: []
 		},
 		{
 			rank: 3,
@@ -60,16 +61,17 @@ export const priestess: SocialLink = {
 				{
 					prompt: 'She thought I was a robot...',
 					options: [
-						{ text: "Don't let it get to you.", points: 2 },
-						{ text: 'You can change.', points: 3 }
+						{ text: 'You can change.', points: 3 },
+						{ text: "Don't let it get to you.", points: 2 }
 					]
 				},
 				{
-					prompt: 'Phone (P5R): She even has a bunch of Buchimaru-kun emoticons!',
-					options: [{ text: 'Sounds like you two get along.', points: 3 }]
+					prompt:
+						'It was crazy to see a picture of her entire collection. She even has a bunch of Buchimaru-kun emoticons!',
+					options: [{ text: 'Sounds like you two get along.', points: 2 }]
 				}
 			],
-			unlocks: ['Brainiac Talk']
+			unlocks: ['Black Belt Talk']
 		},
 		{
 			rank: 4,
@@ -91,8 +93,8 @@ export const priestess: SocialLink = {
 				{
 					prompt: 'I had completely forgotten about that dream, to be honest...',
 					options: [
-						{ text: 'I like a woman in uniform.', points: 2 },
-						{ text: "That's an amazing goal.", points: 3 }
+						{ text: "It's an amazing goal.", points: 3 },
+						{ text: 'I like a woman in uniform.', points: 2 }
 					]
 				}
 			],
@@ -103,10 +105,11 @@ export const priestess: SocialLink = {
 			requirements: '',
 			choices: [
 				{
-					prompt: 'She even sent me a picture of their date at Destinyland.',
+					prompt:
+						"Since then, he's the only thing she'll talk about. She even sent me a picture of their date at Destinyland.",
 					options: [
-						{ text: "That's annoying.", points: 2 },
-						{ text: 'He sounds suspicious.', points: 3 }
+						{ text: 'He sounds suspicious.', points: 3 },
+						{ text: "That's annoying.", points: 2 }
 					]
 				},
 				{
@@ -117,8 +120,8 @@ export const priestess: SocialLink = {
 					]
 				},
 				{
-					prompt: 'Phone (P5R): What should we do…?',
-					options: [{ text: 'Try to reach out to her.', points: 3 }]
+					prompt: 'But if it turns out he really is toying with Eiko... what should we do...?',
+					options: [{ text: 'Try to reach out to her.', points: 2 }]
 				}
 			],
 			unlocks: []
@@ -130,12 +133,13 @@ export const priestess: SocialLink = {
 				{
 					prompt: "Are you seriously datin'?",
 					options: [
-						{ text: 'We just started.', points: 2 },
-						{ text: 'Love comes in many forms.', points: 3 }
+						{ text: 'Love comes in many forms.', points: 3 },
+						{ text: 'We just started.', points: 2 }
 					]
 				},
 				{
-					prompt: 'Phone (P5R): I want to ignore him, but I shouldn’t for Eiko’s sake…',
+					prompt:
+						"He uses these weird symbols - I have no idea what he's saying. I want to ignore him, but I shouldn't for Eiko's sake...",
 					options: [{ text: "Tell him you're on a date.", points: 2 }]
 				}
 			],
@@ -150,12 +154,13 @@ export const priestess: SocialLink = {
 						"I'm not sure any of that would have happened if not for the incident with my father...",
 					options: [
 						{ text: "That's a horrible story.", points: 3 },
-						{ text: 'Was it tough without him?', points: 2 },
-						{ text: 'He was a noble man.', points: 3 }
+						{ text: 'He was a noble man.', points: 3 },
+						{ text: 'Was it tough without him?', points: 2 }
 					]
 				},
 				{
-					prompt: 'Trying to bring justice and order to the world around him.',
+					prompt:
+						'That was how my father chose to live his life... Trying to bring justice and order to the world around him.',
 					options: [
 						{ text: "That's admirable.", points: 3 },
 						{ text: "I'm sure he was happy.", points: 3 }
@@ -169,17 +174,18 @@ export const priestess: SocialLink = {
 					]
 				},
 				{
-					prompt: 'Wanna hav a double wedding?',
+					prompt: "'U serious abt <protagonist>-kun?' 'Wanna hav a double wedding?'",
 					options: [
 						{ text: "I'm game if you are.", points: 2 },
 						{ text: 'Refuse her, please.', points: 2 }
 					]
 				},
 				{
-					prompt: 'Phone (P5R): Can we keep this up until I can make a call one way or the other?',
+					prompt:
+						"I don't trust Eiko's boyfriend yet, so... can we keep this up until I can make a call one way or the other?",
 					options: [
-						{ text: 'Of course.', points: 2 },
-						{ text: "It's the role I always hoped for.", points: 3 }
+						{ text: 'Of course..', points: 2 },
+						{ text: "It's the role I always hoped for.", points: 2 }
 					]
 				}
 			],
@@ -190,9 +196,9 @@ export const priestess: SocialLink = {
 			requirements: '',
 			choices: [
 				{
-					prompt: 'So he calls you his special princess... What do you think, Joker?',
+					prompt: 'So he calls you his special princess... What do you think, <protagonist>-kun?',
 					options: [
-						{ text: 'He says that to all the girls.', points: 2 },
+						{ text: 'He says that to all his girls.', points: 2 },
 						{ text: "That's how he ropes you in.", points: 2 }
 					]
 				},
@@ -200,7 +206,7 @@ export const priestess: SocialLink = {
 					prompt: "She needs to realize that. You're going to help, right?",
 					options: [
 						{ text: 'Absolutely.', points: 3 },
-						{ text: 'What are you going to do?', points: 2 },
+						{ text: 'What are we going to do?', points: 2 },
 						{ text: 'Eh, she deserves him.', points: 2 }
 					]
 				}
@@ -210,7 +216,7 @@ export const priestess: SocialLink = {
 		{
 			rank: 9,
 			requirements:
-				'Get information on the host Tsukasa by talking twice to the man outside the Shinjuku bookstore.',
+				'In Shinjuku, talk to the irritated promoter in front of the bookstore and ask "Do you know a Tsukasa?" (unavailable when raining).',
 			choices: [
 				{
 					prompt: 'I dare you!',
@@ -223,16 +229,16 @@ export const priestess: SocialLink = {
 				{
 					prompt: 'People noticed what happened...',
 					options: [
-						{ text: 'We should get out of here.', points: 2 },
-						{ text: 'Follow me.', points: 0 }
+						{ text: 'We should get out of here.', points: 3 },
+						{ text: 'Follow me.', points: 3 }
 					]
 				},
 				{
-					prompt: "I can't believe I actually hit one of my friends..",
-					options: [{ text: 'That was the right move.', points: 2 }]
+					prompt: "I can't believe I actually hit one of my friends...",
+					options: [{ text: 'That was the right move.', points: 3 }]
 				},
 				{
-					prompt: 'How did you ever find somewhere like this...? You know so much.',
+					prompt: 'How did you ever find somewhere like this? You know so much.',
 					options: [
 						{ text: "I'm a regular here.", points: 3 },
 						{ text: "It's a popular meeting spot.", points: 3 },
@@ -242,27 +248,27 @@ export const priestess: SocialLink = {
 				{
 					prompt: "I can't learn about romance by myself...",
 					options: [
-						{ text: "You'll find someone someday. (Friendship)", points: 3 },
-						{ text: "I'll be your study partner. (Romance)", points: 0 }
+						{ text: "You'll find someone someday.", points: 3, routeFlag: 'friendship' },
+						{ text: "I'll be your study partner.", points: 0, routeFlag: 'romance' }
 					]
 				},
 				{
 					prompt: 'Do you... like me?',
+					route: 'romance',
 					options: [
-						{ text: 'I do. (Romance)', points: 3 },
-						{ text: "That's not what I meant. (Friendship)", points: 0 }
+						{ text: 'I do.', points: 3, routeFlag: 'romance' },
+						{ text: "That's not what I meant.", points: 0, routeFlag: 'friendship' }
 					]
 				},
 				{
-					prompt: 'Phone (P5R, Romance)',
-					options: [{ text: "I'd do anything for you.", points: 2 }]
+					prompt: "I'd probably never have pulled this off alone. Thank you so much.",
+					route: 'friendship',
+					options: [{ text: 'No problem at all.', points: 3 }]
 				},
 				{
-					prompt: 'Phone (P5R, Friendship)',
-					options: [
-						{ text: 'No problem at all.', points: 2 },
-						{ text: 'No big deal.', points: 2 }
-					]
+					prompt: "I'd probably never have pulled this off alone. Thank you so much.",
+					route: 'romance',
+					options: [{ text: "I'd do anything for you.", points: 3 }]
 				}
 			],
 			unlocks: ['Protect']
@@ -270,8 +276,44 @@ export const priestess: SocialLink = {
 		{
 			rank: 10,
 			requirements: '',
-			choices: [],
-			unlocks: ['Cybele fusion', 'Second Awakening (Anat)']
+			choices: [
+				{
+					prompt:
+						'I think she knew it deep within her heart, and seeing him like that finally helped her realize.',
+					options: [{ text: "That's incredible.", points: 3 }]
+				},
+				{
+					prompt: 'I plan on doing the same as well.',
+					options: [{ text: 'Are you stressing over exams?', points: 3 }]
+				},
+				{
+					prompt: 'My dream... is to become a police commissioner. That is why I need to study.',
+					options: [{ text: 'That sounds difficult.', points: 3 }]
+				},
+				{
+					prompt:
+						'Instead, I want to head an organization that will destroy the lawless and help rescue victims.',
+					options: [
+						{ text: 'What an admirable goal.', points: 3 },
+						{ text: 'Your father would be proud.', points: 3 },
+						{ text: "That's an amazing dream.", points: 2 }
+					]
+				},
+				{
+					prompt: 'Must have been difficult babysitting the uptight Miss President, right?',
+					route: 'friendship',
+					options: [
+						{ text: 'We went through so much...', points: 3 },
+						{ text: 'Maybe a bit.', points: 2 }
+					]
+				},
+				{
+					prompt:
+						'*giggle* What a funny thought... Studying to become a head of police while working as a thief...',
+					options: [{ text: 'Just be careful, okay?', points: 3 }]
+				}
+			],
+			unlocks: ['Second Awakening (Anat)', 'Cybele fusion']
 		}
 	]
 };
