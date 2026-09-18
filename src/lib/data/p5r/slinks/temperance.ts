db9995e7010d2bfabefc234ed3dd0e41f9538ff5
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-09-18 16:39:29 +0300

    Fixed Temperance slink (P5R)

--- src/lib/data/p5r/slinks/temperance.ts
+++ src/lib/data/p5r/slinks/temperance.ts
@@ -7,11 +7,11 @@ export const temperance: SocialLink = {
 		value: 'temperance'
 	},
 	name: 'Sadayo Kawakami',
-	location: 'Leblanc (call from the phone)',
+	location: 'Leblanc (yellow phone)',
 	unlock:
-		'- On 5/24, go to the maid-request event with Ryuji. Afterwards, talk to Kawakami in front of the Faculty Lounge for her number, then call her from Leblanc.\n- Guts 3 (Staunch)',
+		'- On 5/24, go to the maid-request event with Ryuji. Afterward, talk to Kawakami in the 2F hallway for her number, then call her from the yellow phone in Leblanc.\n- Guts 3 (Staunch)',
 	availability:
-		'Friday and Saturday nights (also rainy nights). Call from the phone in Leblanc (5,000 yen until Rank 9). Must be maxed by November 18.',
+		'Friday and Saturday nights (also rainy nights). Call from the yellow phone in Leblanc (5,000 yen until Rank 9). Must be maxed by November 18.',
 	ranks: [
 		{
 			rank: 1,
@@ -34,7 +34,7 @@ export const temperance: SocialLink = {
 			requirements: '',
 			choices: [
 				{
-					prompt: "Doesn't it pain your heart to see such a cute maid all dirty?",
+					prompt: "But then, doesn't it pain your heart to see such a cute maid all dirty?",
 					options: [{ text: 'It does.', points: 2 }]
 				},
 				{
@@ -42,7 +42,7 @@ export const temperance: SocialLink = {
 					options: [{ text: 'Absolutely not!', points: 2 }]
 				},
 				{
-					prompt: 'Phone (P5R): Is that okay, Master?',
+					prompt: 'I really wanna do a lot more cleaning for you... Is that okay, Master?',
 					options: [
 						{ text: 'You have the wrong number.', points: 2 },
 						{ text: 'Uh, what the hell?', points: 2 },
@@ -57,7 +57,7 @@ export const temperance: SocialLink = {
 			requirements: '',
 			choices: [
 				{
-					prompt: 'The high-end cup noodles really do make a difference, don’t they?',
+					prompt: "The high-end cup noodles really do make a difference, don't they?",
 					options: [
 						{ text: 'Yeah, they do.', points: 2 },
 						{ text: 'Do some real cooking.', points: 2 }
@@ -68,7 +68,7 @@ export const temperance: SocialLink = {
 					options: [{ text: "I'll request you more often.", points: 3 }]
 				},
 				{
-					prompt: "Phone (P5R): You're still young, so you really need to eat properly...",
+					prompt: "...Seriously, though. You're still young, so you really need to eat properly...",
 					options: [{ text: 'Thanks.', points: 2 }]
 				}
 			],
@@ -112,7 +112,8 @@ export const temperance: SocialLink = {
 					]
 				},
 				{
-					prompt: "Phone (P5R): Do you get to eat that place's curry every day?",
+					prompt:
+						"There's always this nice curry smell around the place. Do you get to eat that place's curry every day?",
 					options: [{ text: 'You want some?', points: 2 }]
 				}
 			],
@@ -129,18 +130,17 @@ export const temperance: SocialLink = {
 				{
 					prompt: "I can't stop coughing...",
 					options: [
-						{ text: 'Are you alright?', points: 3 },
+						{ text: 'Are you all right?', points: 3 },
 						{ text: 'You should go home.', points: 3 },
 						{ text: 'Have you seen a doctor?', points: 2 }
 					]
 				},
 				{
-					prompt:
-						'Phone (P5R): If you neglect stuff like that, it makes you more likely to get sick.',
-					options: [{ text: 'How are you feeling, though?', points: 2 }]
+					prompt: 'If you neglect stuff like that, it makes you more likely to get sick.',
+					options: [{ text: 'How are you feeling, though?', points: 3 }]
 				}
 			],
-			unlocks: []
+			unlocks: ['Super Housekeeping']
 		},
 		{
 			rank: 7,
@@ -155,23 +155,24 @@ export const temperance: SocialLink = {
 				},
 				{
 					prompt: 'I guess transferring to our sister company is the only way...',
-					options: [{ text: 'Think this through more.', points: 3 }]
+					options: [{ text: 'Think this through more.', points: 3, routeFlag: 'romance' }]
 				}
 			],
-			unlocks: ['Super Housekeeping']
+			unlocks: []
 		},
 		{
 			rank: 8,
 			requirements: '',
 			choices: [
 				{
-					prompt: 'This is just what I have to do.',
-					options: [{ text: 'Is this what you really want?', points: 3 }]
+					prompt: '...So this is just what I have to do.',
+					options: [{ text: 'Is this really what you want?', points: 3 }]
 				},
 				{
-					prompt: 'What do you think?',
+					prompt:
+						"That's why I'm going to quit both my teaching job and my maid job... What do you think?",
 					options: [
-						{ text: "If that's what you've decided.", points: 3 },
+						{ text: "If that's what you decided.", points: 3 },
 						{ text: "You're running away.", points: 2 }
 					]
 				},
@@ -184,22 +185,28 @@ export const temperance: SocialLink = {
 					]
 				},
 				{
-					prompt: 'As your teacher, I will never betray you.',
+					prompt:
+						"But from now on, I'm going to protect you too. As your teacher, I will never betray you.",
 					options: [
 						{ text: 'I want to protect you.', points: 3 },
 						{ text: "That's reassuring.", points: 3 }
 					]
 				},
 				{
-					prompt: "Phone (P5R): It's thanks to you that I actually figured out what I need to do.",
-					options: [{ text: "You're welcome.", points: 2 }]
+					prompt:
+						"But I felt so stuck facing it alone. It's thanks to you that I actually figured out what I need to do.",
+					options: [
+						{ text: "You're welcome.", points: 2 },
+						{ text: 'So much for being a maid.', points: 2 },
+						{ text: "I'll do anything for you.", points: 2 }
+					]
 				}
 			],
 			unlocks: ['Mementos request: A Teacher Maid to Suffer']
 		},
 		{
 			rank: 9,
-			requirements: 'Complete the Mementos request A Teacher Maid to Suffer.',
+			requirements: 'Complete Mementos request "A Teacher Maid to Suffer".',
 			choices: [
 				{
 					prompt: "I bet I'll be an old woman by the time I achieve my dreams.",
@@ -216,25 +223,29 @@ export const temperance: SocialLink = {
 					prompt: "Besides, won't you be lonely? Hm?",
 					options: [
 						{ text: 'Not really.', points: 2 },
-						{ text: 'I want to keep seeing you.', points: 2 }
+						{ text: 'I want to keep seeing you.', points: 2, routeFlag: 'romance' }
 					]
 				},
 				{
-					prompt: "Giving someone the wrong idea... It's a sinful act, you know?",
+					prompt: "You don't want to give anybody the wrong idea. That could really hurt someone.",
+					route: 'romance',
 					options: [
 						{ text: 'Got it.', points: 2 },
-						{ text: 'I mean what I say.', points: 0 }
+						{ text: 'I mean what I say.', points: 0, routeFlag: 'romance' }
 					]
 				},
 				{
 					prompt: "This situation isn't normal. I'm a teacher, and you're my student, you know?",
+					route: 'romance',
 					options: [
-						{ text: "I'm a master, you're a maid.", points: 0 },
-						{ text: "I'm a man, you're a woman.", points: 0 }
+						{ text: "I'm a master; you're a maid.", points: 0, routeFlag: 'friendship' },
+						{ text: "I'm a man; you're a woman.", points: 0, routeFlag: 'romance' }
 					]
 				},
 				{
-					prompt: 'Phone (P5R if not romanced):',
+					prompt:
+						'But somewhere along the way, I forgot about all that... Reigniting your original resolve is easier said than done.',
+					route: 'friendship',
 					options: [{ text: 'You can do it.', points: 2 }]
 				}
 			],
@@ -243,8 +254,39 @@ export const temperance: SocialLink = {
 		{
 			rank: 10,
 			requirements: '',
-			choices: [],
-			unlocks: ['Special Massage', 'Ardha fusion']
+			choices: [
+				{
+					prompt:
+						'I need to work hard so I can make up for all the times I let my students down in the past.',
+					route: 'friendship',
+					options: [
+						{ text: "I'm glad to hear that.", points: 3 },
+						{ text: "Sounds like you're busy.", points: 2 }
+					]
+				},
+				{
+					prompt:
+						'I put up with the mess because you were my client, but you need to thoroughly clean in here!',
+					route: 'friendship',
+					options: [{ text: 'I will.', points: 2 }]
+				},
+				{
+					prompt: "A new path where I'll never abandon any of my students...",
+					options: [
+						{ text: 'That sounds promising.', points: 3 },
+						{ text: "I'll make sure of it.", points: 3 }
+					]
+				},
+				{
+					prompt: "I'll obey them, you know... even though I'm no longer a maid.",
+					route: 'romance',
+					options: [
+						{ text: 'I want to rely on you.', points: 3 },
+						{ text: 'I want you to rely on me.', points: 3 }
+					]
+				}
+			],
+			unlocks: ['Ardha fusion', 'Special Massage']
 		}
 	]
 };
